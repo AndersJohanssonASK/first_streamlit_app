4ee47c5719b712dbbbefe356ba97da088ea1a1f7
AndersJohanssonASK <135115973+AndersJohanssonASK@users.noreply.github.com>
2023-08-18 17:10:01 +0200

    Update streamlit_app.py
    added try: block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,27 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice: 
+    streamlit.error("Please select a fruit to get information.")
+  else: 
+    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+    # creates a flat table
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    #display dataframe
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 #streamlit.text(fruityvice_response)
 
 # write your own comment -what does the next line do?
-# creates a flat table
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
+
 # write your own comment - what does this do?
-#display dataframe
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 
